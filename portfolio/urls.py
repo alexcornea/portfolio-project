60ac8da54ef4f3ec37bc37751c1b7be2fab2a166
Alex Cornea <alexcornea@yahoo.com>
2019-09-30 11:00:12 +0100

    requirements

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    url('admin/', admin.site.urls),
+    url('', jobs.views.home, name='home'),
+    url('blog/', include('blog.urls')),
         ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
